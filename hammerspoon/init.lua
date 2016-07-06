ab97ef7b5609c94c546816fb876387dc4634e0d9
Sergey Skovorodnikov <sg.skov@gmail.com>
2016-07-06 12:28:06 -0700

    Update hammerspoon to put windows in corners.

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,5 +1,48 @@
 hs.window.animationDuration = 0
 
+function moveAndResize(scaleX, scaleY, posX, posY)
+    -- window
+    local win = hs.window.focusedWindow()
+    -- frame of focused window
+    local f = win:frame()
+    -- screen of window
+    local screen = win:screen()
+    -- screen frame without dock or menu, its height.
+    -- its a rectangle, hs.geometry class
+    local max = screen:frame()
+
+    -- Window size 
+    f.w = max.w * (scaleX or 1)
+    f.h = max.h * (scaleY or 1)
+
+    -- Window Position
+    --  = size  + (width * (desired position or 0))
+    f.x = max.x + (max.w * (posX or 0))
+    f.y = max.y + (max.h * (posY or 0))
+    win:setFrame(f)
+end
+
+-- move to top left corner
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "U", function()
+  moveAndResize(1/2, 1/2, 0, 0)
+end)
+
+-- move to bottom left corner
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "N", function()
+  moveAndResize(1/2, 1/2, 0, 1/2)
+end)
+
+-- move to top right corner
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "I", function()
+  moveAndResize(1/2, 1/2,  1/2, 0)
+end)
+
+-- move to bottom right corner
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "M", function()
+  moveAndResize(1/2, 1/2,  1/2, 1/2)
+end)
+
+-- align right 1/2 screen full height
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Right", function()
     local win = hs.window.focusedWindow()
     local f = win:frame()
@@ -13,6 +56,7 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Right", function()
     win:setFrame(f)
 end)
 
+-- align left 1/2 screen full height
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Left", function()
     -- window
     local win = hs.window.focusedWindow()
@@ -20,7 +64,7 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Left", function()
     local f = win:frame()
     -- screen of window
     local screen = win:screen()
-    -- scren frame without dock or menu, its height.
+    -- screen frame without dock or menu, its height.
     -- its a rectangle, hs.geometry class
     local max = screen:frame()
 
@@ -31,6 +75,7 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Left", function()
     win:setFrame(f)
 end)
 
+-- take all screen space 
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Up", function()
     -- window
     local win = hs.window.focusedWindow()
@@ -74,6 +119,8 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "k", function()
     local win = hs.window.focusWindowNorth()
 end)
 
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "r", function()
+-- reload config
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", function()
     hs.reload()
+    hs.alert.show("Config reloaded")
 end)
